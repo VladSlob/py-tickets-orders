41c6e818e1599b6ffa30cc725bb23605fd918fed
VladSlob <141657806+VladSlob@users.noreply.github.com>
2025-04-21 14:37:47 +0300

    urls.py

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,21 +1,10 @@
 from django.urls import path, include
-from rest_framework import routers
+from rest_framework.routers import DefaultRouter
+from cinema.views import OrderViewSet
 
-from cinema.views import (
-    GenreViewSet,
-    ActorViewSet,
-    CinemaHallViewSet,
-    MovieViewSet,
-    MovieSessionViewSet,
-)
+router = DefaultRouter()
+router.register(r'orders', OrderViewSet, basename='order')
 
-router = routers.DefaultRouter()
-router.register("genres", GenreViewSet)
-router.register("actors", ActorViewSet)
-router.register("cinema_halls", CinemaHallViewSet)
-router.register("movies", MovieViewSet)
-router.register("movie_sessions", MovieSessionViewSet)
-
-urlpatterns = [path("", include(router.urls))]
-
-app_name = "cinema"
+urlpatterns = [
+    path('', include(router.urls)),
+]
