d8c2ebfaee9b140c31014e30764721b6c609fd85
VladSlob <141657806+VladSlob@users.noreply.github.com>
2025-04-21 14:46:28 +0300

    urls.py

--- cinema/urls.py
+++ cinema/urls.py
@@ -2,6 +2,8 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from cinema.views import OrderViewSet
 
+app_name = "cinema"  # Add this line
+
 router = DefaultRouter()
 router.register(r"orders", OrderViewSet, basename="order")
 
